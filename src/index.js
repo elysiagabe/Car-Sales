8c9199db4f146b16c8cd51fa0ca4bf9dda1b2d84
Elysia Gabe <elysiagabe@gmail.com>
2020-03-17 12:44:46 -0700

    Removes state from App.js & sets up Redux store in index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+// Reducer
+import { featureReducer } from './reducers/featureReducer';
+// Styling
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+const store = createStore(featureReducer);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    rootElement
+);
